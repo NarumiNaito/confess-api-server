d4f36f16f7c922c1f6a85f60c76b7b96865865fb
Naito <naito01030103@icloud.com>
2024-12-18 23:38:29 +0900

    デプロイ修正

--- config/cors.php
+++ config/cors.php
@@ -23,7 +23,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ["https://confession.zangenoyakata.com"],
+    'allowed_origins' => ["https://confess.zangenoyakata.com"],
 
     'allowed_origins_patterns' => [],
 
