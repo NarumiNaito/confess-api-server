2cb19cae6c8b2f4587532f61851a60a240e15719
Naito <naito01030103@icloud.com>
2024-12-19 00:27:06 +0900

    test

--- config/cors.php
+++ config/cors.php
@@ -23,7 +23,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ["https://confess.zangenoyakata.com"],
+    'allowed_origins' => ["*"],
 
     'allowed_origins_patterns' => [],
 
